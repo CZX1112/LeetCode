8fd058c6f56997904757f45bb36d270f45bd8b1c
CZX1112 <544151281@qq.com>
2023-08-11 23:39:27 +0800

    add test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+git config --global --unset http.proxy
+git config --global --unset https.proxy
+git config --global http.proxy http://127.0.0.1:7890
+git config --global https.proxy http://127.0.0.1:7890
+
+# git config --global http.proxy https://127.0.0.1:7890
+# git config --global https.proxy https://127.0.0.1:7890
+
+# git config --global http.proxy http://127.0.0.1:1080
+# git config --global https.proxy http://127.0.0.1:1080
